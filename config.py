02d135984bcf40cfa5e3e4812d1998dc9da566e4
bissness11 <154408458+bissness11@users.noreply.github.com>
2025-02-21 17:21:33 +0500

    Update config.py

--- config.py
+++ config.py
@@ -26,20 +26,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "6534707")
+    API_HASH  = os.environ.get("API_HASH", "4bcc61d959a9f403b2f20149cbbe627a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5225692027:AAFwugDiuhZjwQKUDjWbXhLZlhzzFMXAHJo") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://nhere118:jSsmgEs1uEqnlbrZ@cluster0.rdkih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
+    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/95229620971d4b3db17ff.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "anidbs") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1001729892476"))
     MAX_CONCURRENT_TRANSMISSIONS = int(os.environ.get("MAX_CONCURRENT_TRANSMISSIONS", "2")) # Set the maximum amount of concurrent transmissions (uploads & downloads).
     
     # wes response configuration     
